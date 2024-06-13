13a5b238d8745d4241351b07d15bd3c74ee44d3b
Tikhon Novitskiy <Novitskiy@lanit.ru>
2024-06-13 12:41:59 +0300

    Init commit

---
+++ src/test/kotlin/by/ecomm/ecommcart/ECommCartApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.ecomm.ecommcart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ECommCartApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
