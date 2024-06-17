e764ebfdf6974ab8a72a56c708a9332d3ef13b61
Tikhon Novitskiy <Novitskiy@lanit.ru>
2024-06-17 14:29:29 +0300

    Change api

--- src/main/kotlin/by/ecomm/ecommcart/controller/CartController.kt
+++ src/main/kotlin/by/ecomm/ecommcart/controller/CartController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/cart")
+@RequestMapping("/api/v1/cart")
 class CartController(private val cartService: CartService) {
 
     @GetMapping("/{userId}")
